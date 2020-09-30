4d582ec41633896776b05998813891e21003de95
fanyezhe <fanyezhe@aliyun.com>
2020-09-30 12:40:05 +0800

    decimal to hexadecimal

--- 001/fyz-decimal-to-hexadecimal.c
+++ 001/fyz-decimal-to-hexadecimal.c
@@ -8,7 +8,7 @@
 
 static void decimal_to_hexadecimal(int n, char *p[]){
 
-	if (0 < n){ // 递归先存储十六进制高位
+	if (0 < n){ //递归先存储十六进制高位
 		decimal_to_hexadecimal(n >> 4, p);
 		*(*p)++ = FYZ_HEXADECIMAL_STRING[0xF & n];	
 	}
@@ -16,21 +16,21 @@ static void decimal_to_hexadecimal(int n, char *p[]){
 
 extern char* fyz_decimal_to_hexadecimal(int decimal, char buf[]){
 	
-	buf[0] = '0'; buf[1] = 'x'; // 设置十六进制前缀
+	buf[0] = '0'; buf[1] = 'x'; //设置十六进制前缀
 
-	if (0 < decimal){ // 不小于0时用递归处理
+	if (0 < decimal){ //不小于0时用递归处理
 		char *ptr = buf + 2;
 		decimal_to_hexadecimal(decimal, &ptr);
 		*ptr = '\0';
 
-	} else if (0 > decimal){ // 小于0时用循环处理
+	} else if (0 > decimal){ //小于0时用循环处理
 		char *p = buf + 2;
 		for (int i = decimal, n = sizeof(decimal) * 8; 0 < n; ++p){
 			*p = FYZ_HEXADECIMAL_STRING[0xF & (i >> (n -= 4))];
 		}
 		*p = '\0';
 
-	}else{ // dacimal等于0时...
+	} else{ //dacimal等于0时...
 		buf[2] = '0', buf[3] = '\0';
 	}
 	
--- 001/main.c
+++ 001/main.c
@@ -10,18 +10,18 @@
 
 int main(int argc, char *argv[]){
 	
-	if (2 > argc){ // 未输入参数则退出
+	if (2 > argc){ //若未输入参数则退出
 		return 0;
 	}
 	
-	char buf[32]; // 用于保持转换后的十六进制字符串
+	char buf[32]; //用于保持转换后的十六进制字符串
 
-	// 将输入的十进制参数循环转换并输出
+	//将输入的十进制参数循环转换并输出
 	for (int i = 1, n; i < argc; ++i){
-		n = atoi(argv[i]); // 将字符串转换为十进制
-		// 转换并输出十六进制字符串
+		n = atoi(argv[i]); //将字符串转换为十进制
+		//转换并输出十六进制字符串
 		printf("decimal %d to hexadecimal is " "%s"  ".\n", n, 
-						fyz_decimal_to_hexadecimal(n, buf));				
+					fyz_decimal_to_hexadecimal(n, buf));				
 	}
 
 	return 0;
