5bd6ba0a37cc50f781b0a0c70bc6eba1645d589f
fanyezhe <fanyezhe@aliyun.com>
2020-10-02 09:48:52 +0800

    any base to decimal

--- 003/main.c
+++ 003/main.c
@@ -10,7 +10,7 @@
 
 int main(int argc, char *argv[]){
 
-	if (3 != argc){ // 输入参数数量不对则退出
+	if (2 > argc){ // 输入参数数量不对则退出
 		printf("input error\n");
 		return 0;
 	}
@@ -18,14 +18,15 @@ int main(int argc, char *argv[]){
 	// decimal用于存储转换后的十进制数值, base欲转换进制的说明
 	int decimal, base = atoi(argv[1]); 
 	
-	// 若输入无误则转换成功
-	if (0 == fyz_any_base_to_decimal(base, argv[2], &decimal)){
-		printf("[int: %d bit] %s base %s to decimal is %d.\n", 
-					sizeof(int) * 8, argv[1], argv[2], decimal);
+	for (int i = 2; NULL != argv[i]; ++i){
+		// 若输入无误则转换成功
+		if (0 == fyz_any_base_to_decimal(base, argv[i], &decimal)){
+			printf("[int: %d bit] %s base %s to decimal is %d.\n", 
+						sizeof(int) * 8, argv[1], argv[i], decimal);
 
-	} else{ // 输入错误
-		printf("input error\n");
+		} else{ // 输入错误
+			printf("this %s is input error\n", argv[i]);
+		}
 	}
-
 	return 0;
 }
