8a9b388e3fc1a87b9774afd6f45bfa28e6c46078
fanyezhe <fanyezhe@aliyun.com>
2020-10-10 23:11:43 +0800

    assessment results

--- 010/fyz-assessment-results.c
+++ 010/fyz-assessment-results.c
@@ -8,6 +8,26 @@
 
 extern char fyz_assessment_results(int n){
 
+	/* 不知谁快
+
+	if (60 > n){
+		return 'E';
+
+	}else if (70 > n){
+		return 'D';
+
+	}else if (80 > n){
+		return 'C';
+
+	}else if (90 > n){
+		return 'B';
+
+	}else {
+		return 'A';
+	}
+
+	*/
+
 	// 返回分数评级
 	return 'A' + (90 > n) + (80 > n) + (70 > n) + (60 > n);
 }
